43c3442ec65c51c8bfc4f4223a653d896c5ef204
peddrovieira <pe.bonangelo@gmail.com>
2023-03-29 22:30:09 -0300

    Logic feat

--- app/src/main/java/com/ds2/tiptime/MainActivity.kt
+++ app/src/main/java/com/ds2/tiptime/MainActivity.kt
@@ -2,10 +2,37 @@ package com.ds2.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ds2.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calcularButton.setOnClickListener{calculateTip()}
     }
+
+
+    fun calculateTip(){
+        val stringInTextField = binding.priceInput.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.tip_20_percentage -> 0.2
+            R.id.tip_18_percentage -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.simenao.isChecked
+        if (roundUp){
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.gorjetaResultado.text = getString(R.string.quantidade_da_gorjeta, formattedTip)
+    }
+
 }
+
